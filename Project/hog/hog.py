85ad2f7292a5ed42984c760ca590a8b454928e4c
Viepathos <2468047811@qq.com>
2026-02-08 15:40:49 +0800

    Phase1 over!

--- Project/hog/hog.py
+++ Project/hog/hog.py
@@ -22,6 +22,19 @@ def roll_dice(num_rolls, dice=six_sided):
     assert num_rolls > 0, 'Must roll at least once.'
     # BEGIN PROBLEM 1
     "*** YOUR CODE HERE ***"
+    k = 0
+    sum = 0
+    curr = 0
+    pin = False
+    
+    while k < num_rolls :
+        curr = dice()
+        sum += curr
+        if curr == 1:
+            pin = True
+        k += 1
+
+    return 1 if pin else sum
     # END PROBLEM 1
 
 
@@ -34,6 +47,16 @@ def boar_brawl(player_score, opponent_score):
     """
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    rightmost_digit = player_score % 10
+    tens_digit = opponent_score // 10
+
+    if tens_digit // 10 != 0 :
+        tens_digit = tens_digit % 10
+
+    if rightmost_digit == tens_digit :
+        return 1
+    else:
+        return 3 * abs(tens_digit - rightmost_digit)
     # END PROBLEM 2
 
 
@@ -52,6 +75,10 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided):
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    if num_rolls == 0 :
+        return boar_brawl(player_score, opponent_score)
+    else :
+        return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
@@ -74,15 +101,39 @@ def is_prime(n):
     return True
 
 def num_factors(n):
-    """Return the number of factors of N, including 1 and N itself."""
+    """R:eturn the number of factors of N, including 1 and N itself."""
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    if is_prime(n) :
+        return 2
+    
+    elif n == 1 :
+        return 1
+    
+    else :
+        k = 1
+        total = 0 
+        while k * k <= n :
+            if n % k == 0 :
+                if k * k == n :
+                    total += 1
+                else :
+                    total += 2
+            k += 1
+    return total 
     # END PROBLEM 4
 
 def sus_points(score):
     """Return the new score of a player taking into account the Sus Fuss rule."""
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    if num_factors(score) == 3 or num_factors(score) == 4 :
+        while not is_prime(score) :
+            score += 1
+    
+    return score
+            
+
     # END PROBLEM 4
 
 def sus_update(num_rolls, player_score, opponent_score, dice=six_sided):
@@ -91,6 +142,10 @@ def sus_update(num_rolls, player_score, opponent_score, dice=six_sided):
     """
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    score = simple_update(num_rolls, player_score, opponent_score, dice)  
+    
+    return sus_points(score)
+
     # END PROBLEM 4
 
 
@@ -130,6 +185,14 @@ def play(strategy0, strategy1, update,
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    while score0 < goal and score1 < goal :
+        if who == 0 :
+            num_rolls = strategy0(score0, score1)
+            score0 = update(num_rolls, score0, score1, dice)
+        else :
+            num_rolls = strategy1(score1, score0)
+            score1 = update(num_rolls, score1, score0, dice)
+        who = 1 - who
     # END PROBLEM 5
     return score0, score1
 
