515780755fa1b31f732625fcc89aa4085770bc21
Viepathos <2468047811@qq.com>
2026-03-28 15:08:40 +0800

    Lab08_finished

--- Lab/lab08/lab08.py
+++ Lab/lab08/lab08.py
@@ -12,6 +12,13 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
+    if t.label == None:
+        return 1
+    else:
+        for bs in t.branches:
+            cumulative_mul(bs)
+            t.label *= bs.label
+    
 
 
 def prune_small(t, n):
@@ -31,11 +38,11 @@ def prune_small(t, n):
     >>> t3
     Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
     """
-    while ____:
-        largest = max(____, key=____)
+    while len(t.branches) > n:
+        largest = max([bs for bs in t.branches], key=lambda bs: bs.label)
         t.branches.remove(largest)
     for b in t.branches:
-        ____
+        prune_small(b, n)
 
 
 def delete(t, x):
@@ -58,14 +65,13 @@ def delete(t, x):
     Tree(1, [Tree(4), Tree(5), Tree(3, [Tree(6)]), Tree(6), Tree(7), Tree(8), Tree(4)])
     """
     new_branches = []
-    for _________ in ________________:
-        _______________________
+    for b in t.branches:
+        delete(b, x) #先将后代中含有x的删干净
         if b.label == x:
-            __________________________________
+            new_branches.extend(b.branches) #注意此处不是append而是extend，append是直接将括号内的东西赛进去，而括号内的东西是一个列表而不是Tree对象，我们要的是Tree对象
         else:
-            __________________________________
-    t.branches = ___________________
-
+            new_branches.append(b) #由于我们之前递归已经把子代处理好了，此处直接返回即可
+    t.branches = new_branches
 
 def max_path_sum(t):
     """Return the maximum path sum of the tree.
@@ -75,6 +81,16 @@ def max_path_sum(t):
     11
     """
     "*** YOUR CODE HERE ***"
+    sum = []
+    for b in t.branches:
+        if b.is_leaf:
+            result = b.label
+        else:
+            result = b.label + max_path_sum(b.branches)
+        sum.append(result)
+
+    return t.label + max(sum)
+        
 
 
 class Tree:
